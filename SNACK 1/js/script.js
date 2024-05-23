e2eaf6e47227414450b78030742b943c224b07a7
GencoMattia <gencomattia@hotmail.it>
2024-05-23 15:30:10 +0200

    Snack1: Completed. Added guestList array; with forEach added place = index+1

---
+++ SNACK 1/js/script.js
@@ -0,0 +1,53 @@
+const guestsList = [
+    {
+        tableName: "Tavolo Vip",
+        guestName: "Brad Pitt",
+    },
+    {
+        tableName: "Tavolo Vip",
+        guestName: "Johnny Depp",
+    },
+    {
+        tableName: "Tavolo Vip",
+        guestName: "Johnny Depp",
+    },
+    {
+        tableName: "Tavolo Vip",
+        guestName: "Cristiano Ronaldo",
+    },
+    {
+        tableName: "Tavolo Vip",
+        guestName: "Georgina Rodriguez",
+    },
+    {
+        tableName: "Tavolo Vip",
+        guestName: "Chiara Ferragni",
+    },
+    {
+        tableName: "Tavolo Vip",
+        guestName: "George Clooney",
+    },
+    {
+        tableName: "Tavolo Vip",
+        guestName: "Amal Clooney",
+    },
+    {
+        tableName: "Tavolo Vip",
+        guestName: "Fedez",
+    },
+    {
+        tableName: "Tavolo Vip",
+        guestName: "Amadeus",
+    },
+    {
+        tableName: "Tavolo Vip",
+        guestName: "Fiorello",
+    }
+];
+
+guestsList.forEach((guest, index) => {
+    guest.place = index + 1;
+});
+
+console.log(guestsList);
+
