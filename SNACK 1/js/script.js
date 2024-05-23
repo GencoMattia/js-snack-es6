412657d35a9f90591917b7118a4a3b13031f6cfb
GencoMattia <gencomattia@hotmail.it>
2024-05-23 16:24:09 +0200

    snack1: remade the assignmets using array.Map to convert string array into object array

--- SNACK 1/js/script.js
+++ SNACK 1/js/script.js
@@ -1,52 +1,52 @@
-const guestsList = [
-    {
-        tableName: "Tavolo Vip",
-        guestName: "Brad Pitt",
-    },
-    {
-        tableName: "Tavolo Vip",
-        guestName: "Johnny Depp",
-    },
-    {
-        tableName: "Tavolo Vip",
-        guestName: "Johnny Depp",
-    },
-    {
-        tableName: "Tavolo Vip",
-        guestName: "Cristiano Ronaldo",
-    },
-    {
-        tableName: "Tavolo Vip",
-        guestName: "Georgina Rodriguez",
-    },
-    {
-        tableName: "Tavolo Vip",
-        guestName: "Chiara Ferragni",
-    },
-    {
-        tableName: "Tavolo Vip",
-        guestName: "George Clooney",
-    },
-    {
-        tableName: "Tavolo Vip",
-        guestName: "Amal Clooney",
-    },
-    {
-        tableName: "Tavolo Vip",
-        guestName: "Fedez",
-    },
-    {
-        tableName: "Tavolo Vip",
-        guestName: "Amadeus",
-    },
-    {
-        tableName: "Tavolo Vip",
-        guestName: "Fiorello",
-    }
-];
+const guestNameList = ['Brad Pitt', 'Johnny Depp', 'Johnny Depp', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
 
-guestsList.forEach((guest, index) => {
-    guest.place = index + 1;
-});
+// const guestsList = [
+//     {
+//         tableName: "Tavolo Vip",
+//         guestName: "Brad Pitt",
+//     },
+//     {
+//         tableName: "Tavolo Vip",
+//         guestName: "Johnny Depp",
+//     },
+//     {
+//         tableName: "Tavolo Vip",
+//         guestName: "Johnny Depp",
+//     },
+//     {
+//         tableName: "Tavolo Vip",
+//         guestName: "Cristiano Ronaldo",
+//     },
+//     {
+//         tableName: "Tavolo Vip",
+//         guestName: "Georgina Rodriguez",
+//     },
+//     {
+//         tableName: "Tavolo Vip",
+//         guestName: "Chiara Ferragni",
+//     },
+//     {
+//         tableName: "Tavolo Vip",
+//         guestName: "George Clooney",
+//     },
+//     {
+//         tableName: "Tavolo Vip",
+//         guestName: "Amal Clooney",
+//     },
+//     {
+//         tableName: "Tavolo Vip",
+//         guestName: "Fedez",
+//     },
+//     {
+//         tableName: "Tavolo Vip",
+//         guestName: "Amadeus",
+//     },
+//     {
+//         tableName: "Tavolo Vip",
+//         guestName: "Fiorello",
+//     }
+// ];
+
+const guestsList = guestNameList.map((guest, index) => ({tableName: "Tavolo Vip", guestName: guest, place: index + 1}));
 
 console.log(guestsList);
