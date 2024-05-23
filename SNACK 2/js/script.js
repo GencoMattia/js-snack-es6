93f6036af92bddf8aafeba1c27114777d65d9e06
GencoMattia <gencomattia@hotmail.it>
2024-05-23 15:37:30 +0200

    snack2: added studentsList

---
+++ SNACK 2/js/script.js
@@ -0,0 +1,37 @@
+const studentsList = [
+    {
+        id: 213,
+        name: "Giuseppina della Rovere",
+        grades: 78
+    },
+    {
+        id: 110,
+        name: "Giuseppina della Rovere",
+        grades: 78
+    },
+    {
+        id: 250,
+        name: "Giuseppina della Rovere",
+        grades: 78
+    },
+    {
+        id: 145,
+        name: "Giuseppina della Rovere",
+        grades: 78
+    },
+    {
+        id: 196,
+        name: "Giuseppina della Rovere",
+        grades: 78
+    },
+    {
+        id: 102,
+        name: "Giuseppina della Rovere",
+        grades: 78
+    },
+    {
+        id: 120,
+        name: "Giuseppina della Rovere",
+        grades: 78
+    },
+];
