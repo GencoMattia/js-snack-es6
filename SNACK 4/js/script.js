483c0d2113578566002c44ee96aafef453f08aa7
GencoMattia <gencomattia@hotmail.it>
2024-05-24 16:30:12 +0200

    Snack4: added array SquadreCalcio

---
+++ SNACK 4/js/script.js
@@ -0,0 +1,52 @@
+const squadreCalcio = [
+    { 
+        nome: "Juventus", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Inter", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Milan", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Roma", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Napoli", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Lazio", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Fiorentina", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Atalanta", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Torino", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    },
+    { 
+        nome: "Sampdoria", 
+        puntiFatti: 0, 
+        falliSubiti: 0 
+    }
+];
