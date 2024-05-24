d69312eaae198fbefffe7ff75759de4390a4ca86
GencoMattia <gencomattia@hotmail.it>
2024-05-24 17:01:22 +0200

    Snack5: created getRandomAlphabetLetter function

--- SNACK 5/js/script.js
+++ SNACK 5/js/script.js
@@ -20,3 +20,24 @@ const fashionArticlesList = [
         color: "red",
     },
 ];
+
+
+
+
+
+// ----------FUNCTIONS------------
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomAlphabetLetter(){
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const randomNumber = getRandomNumber(0, 25);
+    const randomLetter = alphabet[randomNumber];
+    return randomLetter
+}
+
+
+
+console.log(getRandomAlphabetLetter());
