829cdf4c9e45459e544f40f2bfca2aa8bb563e8a
GencoMattia <gencomattia@hotmail.it>
2024-05-24 15:48:58 +0200

    snack3: added racingBikes array

---
+++ SNACK 3/js/script.js
@@ -0,0 +1,42 @@
+const racingBikes = [
+    { 
+        name: "Bianchi Oltre XR4", 
+        weight: 6.8 
+    },
+    { 
+        name: "Cannondale SuperSix EVO", 
+        weight: 7.1 
+    },
+    { 
+        name: "Specialized S-Works Tarmac", 
+        weight: 6.7 
+    },
+    { 
+        name: "Trek Emonda SLR", 
+        weight: 6.5 
+    },
+    { 
+        name: "Cervelo R5", 
+        weight: 6.9 
+    },
+    { 
+        name: "Pinarello Dogma F12", 
+        weight: 7.0 
+    },
+    { 
+        name: "Giant TCR Advanced SL", 
+        weight: 6.8 
+    },
+    { 
+        name: "Scott Addict RC", 
+        weight: 6.7 
+    },
+    { 
+        name: "Colnago V3Rs", 
+        weight: 6.9 
+    },
+    { 
+        name: "Wilier Triestina Zero SLR", 
+        weight: 6.6 
+    }
+];
